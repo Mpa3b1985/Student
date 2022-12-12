ef08b5bbf36c80b03cec25aaa3cd0902b1b7da32
Your Name <you@example.com>
2022-12-12 17:02:40 +0300

    add d3.5

---
+++ Pr6.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+main()
+{
+	int i=1;
+	while(i<10)
+	{
+		printf("Hello\n");
+		i=i+1;
+	}
+
+	int z=0;
+	while(z<15)
+	{
+		if(z%2==1)
+		{
+			printf("%i\n",z);
+		}
+		z=z+1;
+	}
+
+	int s=5;
+	int sum=0;
+	while(s<15)
+	{
+		sum=sum+s;
+		printf("%i+",s);
+		s=s+1;
+	}
+	sum=sum+s;
+	printf("%i=%i",s,sum);
+}
