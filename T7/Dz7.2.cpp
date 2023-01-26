847c0d4a5fc1b39fde1644e88181dd984c4bc111
Your Name <you@example.com>
2023-01-26 15:18:23 +0300

    add dz 7.2

--- T7/Dz7.2.cpp
+++ T7/Dz7.2.cpp
@@ -24,3 +24,41 @@ int main()
 	farray(arr,SIZE);
 	farray(arr2,SIZE2);
 }
+
+//решение из интернета
+
+#include<iostream>
+#include<time.h>
+using namespace std;
+
+
+void farray(int arr[], const int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		arr[i]=rand() % 10;
+	}
+}
+
+void parray(int arr[], const int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+main()
+{
+	const int SIZE=10;
+	const int SIZE2=15;
+
+	int arr[SIZE];
+	int arr2[SIZE2];
+
+	farray(arr,SIZE);
+	parray(arr,SIZE);
+
+	farray(arr2,SIZE2);
+	parray(arr2,SIZE2);
+}
