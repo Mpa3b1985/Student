62ee1d8e89891eb168d1133d041eabf0ddf90216
Your Name <you@example.com>
2023-02-13 17:38:13 +0300

    add dz 8.2-1

---
+++ T8/Dz8.2-1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int devide (int value)
+{
+    if (value == 0)
+    {
+        throw value;
+    }
+
+    return 1024 / value;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int n;
+
+    cout << "Введите число= " << endl;
+    cin >> n;
+
+    try
+    {
+        cout << devide(n) << endl;
+    }
+    catch (const int ex)
+    {
+        cout << "Число равно " << ex << endl;
+    }
+
+    return 0;
+}
